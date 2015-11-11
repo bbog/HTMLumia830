9f5d177a8adc04e2acb51cb828008c48ac9a3653
Bogdan Bucur <contact@bogdanbucur.eu>
2015-11-11 23:11:15 +0200

    Zero-padding for the screensaver time

--- js/lumia.js
+++ js/lumia.js
@@ -35,6 +35,14 @@ var Lumia = (function () {
 				hour = date.getHours(),
 				minutes = date.getMinutes();
 
+			if (hour < 10) {
+				hour = '0' + hour;
+			}
+
+			if (minutes < 10) {
+				minutes = '0' + minutes;
+			}
+
 			var currentTime = hour + ':' + minutes;
 			timeContainer.innerHTML = currentTime;
 		}
